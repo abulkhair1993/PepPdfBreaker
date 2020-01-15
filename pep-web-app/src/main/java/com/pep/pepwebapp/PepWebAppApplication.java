02a0cdeeb5796032542d65d3da10dfbef4b5afff
Abulkhair shaikh <abulkhair.shaikh93@gmail.com>
2020-01-15 17:20:39 +0530

    first commit

---
+++ pep-web-app/src/main/java/com/pep/pepwebapp/PepWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.pep.pepwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepWebAppApplication.class, args);
+	}
+
+}
---
+++ pep-web-app/src/test/java/com/pep/pepwebapp/PepWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pep.pepwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
